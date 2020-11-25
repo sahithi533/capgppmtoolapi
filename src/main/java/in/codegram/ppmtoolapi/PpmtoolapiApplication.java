c6c2fd62611a5c3c1d82fbcdb750011d623db528
Yerram <sahithi.yerram@capgemini.com>
2020-11-25 13:55:29 +0530

    Initial Commit

---
+++ src/main/java/in/codegram/ppmtoolapi/PpmtoolapiApplication.java
@@ -0,0 +1,12 @@
+package in.codegram.ppmtoolapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmtoolapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpmtoolapiApplication.class, args);
+	}
+}
